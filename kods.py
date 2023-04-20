e4e4ed281aecbd317a0a4f70535a9c95d602b30a
L-Augsstkalnietis <laurisaugstkalnietis@gmail.com>
2023-04-20 08:27:40 +0300

    bg
    pievienoju bg

--- kods.py
+++ kods.py
@@ -1,7 +1,8 @@
 from tkinter import*
 root=Tk()
+root.title('Mentālā veselība')
 garums=800
 platums=500
-a=Canvas(root,width=garums,height=platums)
+a=Canvas(root,width=garums,height=platums,bg='#4d4dff')
 a.pack()
 root.mainloop()

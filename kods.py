5f68f8202dfed2fbf705ae223d03d113683594c7
L-Augsstkalnietis <laurisaugstkalnietis@gmail.com>
2023-04-20 09:34:51 +0300

    jauns apgalvojums

--- kods.py
+++ kods.py
@@ -9,4 +9,5 @@ virsraksts=a.create_text(400,50,text="Mentālā veselība",fill="#660066",font=(
 Apgalvojums1=a.create_text(125,125,text="Cik daudz gulēji?",fill="#660066",font=('Helvetica',20))
 Apgalvojums2=a.create_text(200,210,text="Cik daudz vingro katru dienu?",fill="#660066",font=('Helvetica',20))
 Apgalvojums3=a.create_text(190,295,text="Kāds ir tavs stresa līmenis?",fill="#660066",font=('Helvetica',20))
+Apgalvojums4=a.create_text(165,375,text="Kā tev patīk sava vide?",fill="#660066",font=('Helvetica',20))
 root.mainloop()

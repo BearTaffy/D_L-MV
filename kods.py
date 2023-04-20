8e64314e29f682036071b8f37f17d9f542b69c61
L-Augsstkalnietis <laurisaugstkalnietis@gmail.com>
2023-04-20 08:49:14 +0300

    apgalvojumi

--- kods.py
+++ kods.py
@@ -5,4 +5,8 @@ garums=800
 platums=500
 a=Canvas(root,width=garums,height=platums,bg='#4d4dff')
 a.pack()
+virsraksts=a.create_text(400,50,text="Mentālā veselība",fill="#660066",font=('Helvetica',30))
+Apgalvojums1=a.create_text(125,125,text="Cik daudz gulēji?",fill="#660066",font=('Helvetica',20))
+Apgalvojums2=a.create_text(200,210,text="Cik daudz vingro katru dienu?",fill="#660066",font=('Helvetica',20))
+Apgalvojums3=a.create_text(190,295,text="Kāds ir tavs stresa līmenis?",fill="#660066",font=('Helvetica',20))
 root.mainloop()

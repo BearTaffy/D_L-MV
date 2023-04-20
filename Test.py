9b70df37f1a56dac7abe414a8fc977c1d06d78dd
BearTaffy <81381255+BearTaffy@users.noreply.github.com>
2023-04-20 12:22:23 +0300

    main file from lauris

--- Test.py
+++ Test.py
@@ -1,56 +1,17 @@
 from tkinter import *
+window = Tk()
+window.state('zoomed')
+window.configure(bg = 'green4')
 
-from tkinter import ttk
+def drag(event):
+    event.widget.place(x=event.x_root, y=event.y_root,anchor=CENTER)
 
+card = Canvas(window, width=74, height=97, bg='blue')
+card.place(x=300, y=600,anchor=CENTER)
+card.bind("<B1-Motion>", drag)
 
+another_card = Canvas(window, width=74, height=97, bg='red')
+another_card.place(x=600, y=600,anchor=CENTER)
+another_card.bind("<B1-Motion>", drag)
 
-# root window
-root = Tk()
-root.geometry('600x300')
-root.resizable(False, False)
-
-
-
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=3)
-
-
-# slider value
-current_value = DoubleVar()
-
-
-def get_current_value():
-    return int(current_value.get())
-
-def done():
-    pbar1 = ttk.Progressbar(root, maximum=12, value=get_current_value(), orient='vertical', style='my.Vertical.TProgressbar')
-    pbar1.grid(row=2, column=0, padx=40, pady=10)
-
-
-def slider_changed(event):
-    value_label.configure(text=get_current_value())
-    pbar1.configure(value=get_current_value())
-
-#  slider
-slider = ttk.Scale(root,from_=0,to=12,orient='horizontal',command=slider_changed,variable=current_value)
-
-slider.grid(column=0,row=0,sticky='we')
-
-# value label
-value_label = ttk.Label(root,text=get_current_value())
-value_label.grid(row=1,columnspan=1,sticky='n')
-
-dutton = ttk.Button(root,text="save",command=done())
-dutton.grid(column=5,row=4,sticky='se')
-
-style = ttk.Style(root)
-
-# Progressbar style
-style.configure('my.Vertical.TProgressbar', background='#f7f4bf', troughcolor='#8a7852',pbarrelief='flat', troughrelief='flat')
-
-pbar1 = ttk.Progressbar(root, maximum=12, value=get_current_value(), orient='vertical', style='my.Vertical.TProgressbar')
-pbar1.grid(row=2, column=0, padx=40, pady=10)
-
-
-root.configure()
-root.mainloop()
+window.mainloop()
--- test_main.py
+++ test_main.py
@@ -2,6 +2,7 @@ from tkinter import*
 from tkinter import ttk
 
 root=Tk()
+style = ttk.Style(root)
 root.title('Mentālā veselība')
 garums=800
 platums=500
@@ -19,34 +20,77 @@ root.columnconfigure(1, weight=3)
 
 
 # slider value
-current_value = DoubleVar()
+current_value1 = DoubleVar()
+current_value2 = DoubleVar()
+current_value3 = DoubleVar()
+current_value4= DoubleVar()
 
-
-def get_current_value():
-    return int(current_value.get())
+def get_current_value1():
+    return int(current_value1.get())
+def get_current_value2():
+    return int(current_value2.get())
+def get_current_value3():
+    return int(current_value3.get())
+def get_current_value4():
+    return int(current_value4.get())
 
 
 def slider_changed(event):
-    value_label.configure(text=get_current_value())
-    pbar1.configure(value=get_current_value())
+    value_label1.configure(text=get_current_value1())
+    value_label2.configure(text=get_current_value2())
+    value_label3.configure(text=get_current_value3())
+    value_label4.configure(text=get_current_value4())
+    pbar1.configure(value=get_current_value1())
+    pbar2.configure(value=get_current_value2())
+    pbar3.configure(value=get_current_value3())
+    pbar4.configure(value=get_current_value4())
+
+# Slider style
+style.configure('horzontal_Slider', background='#4d4dff')
+
 
 #  slider
-slider = ttk.Scale(root,from_=0,to=12,orient='horizontal',command=slider_changed,variable=current_value).pack()
+slider1 = ttk.Scale(root,from_=0,to=12,length=150,orient='horizontal',command=slider_changed,variable=current_value1,style='horzontal_Slider')
+slider1.pack()
+a.create_window(125, 175, window=slider1)
 
+slider2 = ttk.Scale(root,from_=0,to=12,length=150,orient='horizontal',command=slider_changed,variable=current_value2,style='horzontal_Slider')
+slider2.pack()
+a.create_window(125, 260, window=slider2)
+
+slider3 = ttk.Scale(root,from_=0,to=12,length=150,orient='horizontal',command=slider_changed,variable=current_value3,style='horzontal_Slider')
+slider3.pack()
+a.create_window(125, 335, window=slider3)
+
+slider4 = ttk.Scale(root,from_=0,to=12,length=150,orient='horizontal',command=slider_changed,variable=current_value4,style='horzontal_Slider')
+slider4.pack()
+a.create_window(125, 420, window=slider4)
 
 
 # value label
-value_label = ttk.Label(root,text=get_current_value())
+value_label1 = ttk.Label(root,text=get_current_value1())
+value_label2 = ttk.Label(root,text=get_current_value2())
+value_label3 = ttk.Label(root,text=get_current_value3())
+value_label4 = ttk.Label(root,text=get_current_value4())
+value_label1.pack(side=LEFT)
+value_label2.pack(side=LEFT)
+value_label3.pack(side=LEFT)
+value_label4.pack(side=LEFT)
 
 
 
-style = ttk.Style(root)
 
 # Progressbar style
-style.configure('my.Vertical.TProgressbar', background='#f7f4bf', troughcolor='#8a7852',pbarrelief='flat', troughrelief='flat')
+style.configure('vertical_ProgressBar', background='#f7f4bf', troughcolor='#8a7852',pbarrelief='flat', troughrelief='flat')
 
-pbar1 = ttk.Progressbar(root, maximum=12, value=get_current_value(), orient='vertical', style='my.Vertical.TProgressbar')
-
+pbar1 = ttk.Progressbar(root, maximum=12, value=get_current_value1(), orient='vertical', style='vertical_ProgressBar')
+pbar1.pack(side=LEFT)
+pbar2 = ttk.Progressbar(root, maximum=12, value=get_current_value2(), orient='vertical', style='vertical_ProgressBar')
+pbar2.pack(side=LEFT)
+pbar3 = ttk.Progressbar(root, maximum=12, value=get_current_value3(), orient='vertical', style='vertical_ProgressBar')
+pbar3.pack(side=LEFT)
+pbar4 = ttk.Progressbar(root, maximum=12, value=get_current_value4(), orient='vertical', style='vertical_ProgressBar')
+pbar4.pack(side=LEFT)
 
 
 root.configure()

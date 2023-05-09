e748e8edf42bd87b87340d1d11c1318dc2835880
L-Augsstkalnietis <laurisaugstkalnietis@gmail.com>
2023-05-09 16:03:22 +0300

    pievienoju jaunu jautājumu

--- kods.py
+++ kods.py
@@ -3,24 +3,24 @@ from tkinter import*
 root=Tk()
 root.title("Mentālā veselība")
 garums=800 #x
-platums=500 #y
-a=Canvas(root,width=garums,height=platums,bg="#4d4dff")
+platums=700 #y
+a=Canvas(root,width=garums,height=platums,bg="#b366ff")
 a.pack()
-infocanva=Canvas(root,width=garums,height=platums,bg="#4d4dff")
+infocanva=Canvas(root,width=garums,height=platums,bg="#b366ff")
 infocanva.pack_forget()
-gamecanva=Canvas(root,width=garums,height=platums,bg="#4d4dff")
+gamecanva=Canvas(root,width=garums,height=platums,bg="#b366ff")
 gamecanva.pack_forget()
 bar_width = 0
 
 # info canva
 virsraksts2=infocanva.create_text(400,50,text="Mentālā veselība",fill="#660066",font=("Helvetica",30))
-go_back=infocanva.create_text(700,480,text="Atgriezties",fill="#660066",font=("Helvetica",20))
+go_back=infocanva.create_text(700,platums-20,text="Atgriezties",fill="#660066",font=("Helvetica",20))
 #info1=infocanva.create_text(400,140,text="Šī spēle ir par mentālo veselību",fill="#660066",font=("Helvetica",20))
 #info2=infocanva.create_text(380,200,text="Šajā spelē parādīts kā dažādi faktori ietekmē mentālo veselību",fill="#660066",font=("Helvetica",20))
 #info3=infocanva.create_text(400,270,text="Kā spēlēt?",fill="#660066",font=("Helvetica",20))
 #info3=infocanva.create_text(370,330,text="Mainot atbildes redzēsi, kā faktori ietekmē mentālo veselību",fill="#660066",font=("Helvetica",20))
 #info4=infocanva.create_text(400,370,text="un šo faktoru sekas",fill="#660066",font=("Helvetica",20))
-rs = infocanva.create_text(400,200, text="Šī spēle ir par mentālo veselību. Šajā spelē parādīts kā dažādi faktori ietekmē mentālo veselību.                     Kā spēlēt?             Mainot atbildes redzēsi, kā faktori ietekmē mentālo veselību un šo faktoru sekas.",width=400, fill="white",font=("helvetica",20))
+rs = infocanva.create_text(400,200, text="Šī spēle ir par mentālo veselību. Šajā spelē parādīts kā dažādi faktori ietekmē mentālo veselību. Kā spēlēt? Mainot atbildes redzēsi, kā faktori ietekmē mentālo veselību un šo faktoru sekas.",width=400, fill="white",font=("helvetica",20))
 
 # home canva
 virsraksts=a.create_text(400,50,text="Mentālā veselība",fill="#660066",font=("Helvetica",30))
@@ -30,15 +30,17 @@ Apgalvojums2_ja=a.create_text(90,240,text="Jā",fill="#660066",font=("Helvetica"
 Apgalvojums2_ne=a.create_text(180,240,text="Nē",fill="#660066",font=("Helvetica",20))
 Apgalvojums3=a.create_text(190,320,text="Kāds ir tavs stresa līmenis?",fill="#660066",font=("Helvetica",20))
 Apgalvojums4=a.create_text(165,400,text="Kā tev patīk sava vide?",fill="#660066",font=("Helvetica",20))
-info_bt=a.create_text(700,480,text="Informācija",fill="#660066",font=("Helvetica",20))
-game_bt=a.create_text(680,450,text="Redzēt rezultātus",fill="#660066",font=("Helvetica",20))
+Apgalvojums5=a.create_text(290,530,text="Cik daudz laika(stundas) ikdienā veltāt sociālajiem medijiem vai digitālajām ierīcēm?",fill="#660066",font=("Helvetica",20),width=600)
+info_bt=a.create_text(700,platums-20,text="Informācija",fill="#660066",font=("Helvetica",20))
+game_bt=a.create_text(680,platums-50,text="Redzēt rezultātus",fill="#660066",font=("Helvetica",20))
 
 # game canva
-go_back_home=gamecanva.create_text(650,480,text="Atpakaļ uz jautājumiem",fill="#660066",font=("Helvetica",20))
-sleep1=gamecanva.create_text(50,50,text="Miegs")
-gym1=gamecanva.create_text(50,75,text="Vingrošana")
-stress1=gamecanva.create_text(50,100,text="Stress")
-vide1=gamecanva.create_text(50,125,text="Vide")
+go_back_home=gamecanva.create_text(650,platums-20,text="Atpakaļ uz jautājumiem",fill="#660066",font=("Helvetica",20))
+sleep1=gamecanva.create_text(70,50,text="Miegs",fill="#660066",font=("Helvetica",20))
+gym1=gamecanva.create_text(70,100,text="Vingrošana",fill="#660066",font=("Helvetica",20))
+stress1=gamecanva.create_text(70,150,text="Stress",fill="#660066",font=("Helvetica",20))
+vide1=gamecanva.create_text(70,200,text="Vide",fill="#660066",font=("Helvetica",20))
+screen_time1=gamecanva.create_text(100,250,text="Digitālās ierīces",fill="#660066",font=("Helvetica",20))
 def callback(event):
     print( "clicked at", event.x, event.y)
 
@@ -80,6 +82,12 @@ def update_bar4(event):
     value = slider4.get()
     bar_width = value * 15
     a.coords(bar4,240, 420, 240 + bar_width, 440)   
+
+def update_bar5(event):
+    global bar_width
+    value = slider5.get()
+    bar_width = value * 15
+    a.coords(bar5,240, 420, 240 + bar_width, 440)   
 # functions that show the slider for gym
 def yes():
     a.itemconfigure(slidder2_s,state="normal")
@@ -136,9 +144,19 @@ def vide():
     elif 2== slider4.get() or slider4.get() <=6:
         stress_tx = gamecanva.create_text(650, 250, text=yellow4, width=200, fill="black", font=("helvetica", 15))
 
+def screen_time():
+    red5="Tev noteikti jāsamazina veltītais laiks ierīcēm! Tu pārsnied vidējo, kas ir septiņas stundas. Tu vari ierobežot savu laiku pieliekot sociālajiem tīkliem noteiktu laika limitu cik tos var izmantot vai arī sāc jaunu hobiju."
+    green5="Tu vari lepoties ar savu atbildi, jo vidējais laiks cik pavada sociālajos tīklos un pie ierīču ekrāniem ir septiņas stundas dienā. "
+    yellow5="Tev vajadzētu samazināt savu laiku sociālajos tīklos un pie ierīču ekrāniem, bet tu nepārsniedz septiņas stundas, kas ir cik vidēji cilvēks velta laiku ierīcēm."
+    block = gamecanva.create_rectangle(550, 50, 750, 450, fill="white")
+    if slider5.get()<=2:
+        screen_time_tx = gamecanva.create_text(650, 250, text=green5, width=200, fill="black", font=("helvetica", 15))
+    elif  2<slider5.get() and slider5.get() <=7:
+        screen_time_tx = gamecanva.create_text(650, 250, text=yellow5, width=200, fill="black", font=("helvetica", 15))
+    elif 7< slider5.get() and slider5.get() >=15:
+        screen_time_tx = gamecanva.create_text(650, 250, text=red5, width=200, fill="black", font=("helvetica", 15))
 
-
-style = {"troughcolor": "#505050", "sliderlength": 30, "sliderrelief": "flat", "background": "#4d4dff"}
+style = {"troughcolor": "#505050", "sliderlength": 30, "sliderrelief": "flat", "background": "#b366ff"}
 
 
 
@@ -172,6 +190,13 @@ a.create_window(125, 445, window=slider4)
 slider4.config(showvalue=True, sliderlength=20,)
 slider4.set(6)
 
+slider5 = Scale(root, from_=0, to=15, orient=HORIZONTAL, length=200, **style)
+slider5.config(troughcolor='#505050', sliderrelief='flat', highlightthickness=0)
+slider5.place(x=100, y=100)
+a.create_window(125, 580, window=slider5)
+slider5.config(showvalue=True, sliderlength=20,)
+slider5.set(6)
+
 
 
 
@@ -180,15 +205,18 @@ bar = a.create_rectangle(240, 175, 240 + bar_width, 170, fill='white', outline='
 bar2= a.create_rectangle(240, 260, 240 + bar_width, 260, fill='white', outline='')
 bar3= a.create_rectangle(240, 335, 240 + bar_width, 335, fill='white', outline='')
 bar4= a.create_rectangle(240, 420, 240 + bar_width, 420, fill='white', outline='')
+bar5= a.create_rectangle(240, 420, 240 + bar_width, 420, fill='white', outline='')
 slider1.bind('<B1-Motion>', update_bar)
 slider2.bind('<B1-Motion>', update_bar2)
 slider3.bind('<B1-Motion>', update_bar3)
 slider4.bind('<B1-Motion>', update_bar4)
+slider5.bind('<B1-Motion>', update_bar5)
 # Hideing the progressbars
 a.itemconfigure(bar,state="hidden")
 a.itemconfigure(bar2,state="hidden")
 a.itemconfigure(bar3,state="hidden")
 a.itemconfigure(bar4,state="hidden")
+a.itemconfigure(bar5,state="hidden")
 # hideing the gym slider
 a.itemconfigure(slidder2_s,state="hidden")
 # Buttons that move from one canvas to another canvas
@@ -204,6 +232,8 @@ gamecanva.tag_bind(sleep1,"<Button-1>",lambda event: sleep())
 gamecanva.tag_bind(gym1,"<Button-1>",lambda event: gym())
 gamecanva.tag_bind(stress1,"<Button-1>",lambda event: stress())
 gamecanva.tag_bind(vide1,"<Button-1>",lambda event: vide())
+gamecanva.tag_bind(screen_time1,"<Button-1>",lambda event: screen_time() )
+
 
 root.configure()
 root.mainloop()
